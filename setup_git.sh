c9a1f254fc8965a674c3db99c56a0fd4f6cb5098
Helmuth <helmuthdu@gmail.com>
2013-07-16 11:24:53 -0300

    easy git setup

---
+++ setup_git.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/sh
+
+NAME=`git config --global user.name`
+EMAIL=`git config --global user.email`
+USER=`git config --global github.user`
+
+[[ -z $NAME ]] && read -p "Nome: " NAME
+[[ -z $EMAIL ]] && read -p "Email: " EMAIL
+[[ -z $USER ]] && read -p "Username: " USER
+
+git config --global user.name $NAME
+git config --global user.email $EMAIL
+git config --global github.user $USER
+git config --global color.ui true
+git config --global push.default matching
+git config --global core.editor "vim"
+git config --global --list
+ORIGIN=`git remote`
+if [[ -z $ORIGIN ]]; then
+  read -p "Repository [ex: xoox/nipples]: " REPOSITORY
+  git remote add origin git@github.com:${REPOSITORY}.git
+  git pull origin master
+  git remote set-url --push origin git@github.com:${REPOSITORY}.git
+else
+  git remote set-url --push origin git@github.com:${USER}/${PWD##*/}.git
+fi
