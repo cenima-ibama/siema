f3aea624187774adf72ae9d32c110aecaf59a588
Helmuth <helmuthdu@gmail.com>
2013-08-29 16:01:31 -0300

    development build

---
+++ application/views/pages/form_fail.php
@@ -0,0 +1,3 @@
+<div class="alert alert-error fade in">
+    <strong>Formulário não existente no banco!</strong>
+</div>
---
+++ application/views/templates/form_fail.php
@@ -0,0 +1,3 @@
+<?php $this->load->view('includes/header'); ?>
+<?php $this->load->view('pages/form_fail'); ?>
+<?php $this->load->view('includes/footer'); ?>
