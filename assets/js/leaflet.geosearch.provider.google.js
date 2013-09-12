47a5f71201fbce0ba7a4219621bfcf9d27149386
Helmuth <helmuthdu@gmail.com>
2013-09-12 16:07:55 -0300

    update leaflet.geosearch plugin

--- assets/js/leaflet.geosearch.provider.google.js
+++ assets/js/leaflet.geosearch.provider.google.js
@@ -24,27 +24,28 @@ L.GeoSearch.Provider.Google = L.Class.extend({
 
 	GetLocations: function(qry, callback) {
 		var geocoder = L.GeoSearch.Provider.Google.Geocoder;
-		
+
 		var parameters = L.Util.extend({
 			address: qry
 		}, this.options);
 
-		var results = geocoder.geocode(parameters, function(data){
+		var results = geocoder.geocode(parameters, function (data){
 			data = {results: data};
 
-			if (data.results.length == 0)
-				return [];
+			// if (data.results.length == 0)
+			// 	return [];
 
 			var results = [];
 			for (var i = 0; i < data.results.length; i++)
 				results.push(new L.GeoSearch.Result(
-					data.results[i].geometry.location.lng(), 
-					data.results[i].geometry.location.lat(), 
+					data.results[i].geometry.location.lng(),
+					data.results[i].geometry.location.lat(),
 					data.results[i].formatted_address
 				));
 
 			if(typeof callback == 'function')
 				callback(results);
+			// return results;
 		});
 	},
 });
