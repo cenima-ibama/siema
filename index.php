b9f01f43d9118579ed8af5692cc4214b8db48675
Caio Castro <caio.castro@hexgis.com>
2014-10-10 14:47:34 -0300

    merging develop code into master code

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+
+
 <?php
 /**
  * CodeIgniter
@@ -42,8 +44,8 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
-	// define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'production');
+//	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'production');
 
 /*
  *---------------------------------------------------------------
