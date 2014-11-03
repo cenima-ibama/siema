cb47ba474c0329d98a11ccfdc61f27d3ff9f6db9
Caio Castro <caio.castro@hexgis.com>
2014-11-03 09:16:46 -0200

    added folder for upload of files

---
+++ scripts/indexing_relation_tables.sql
@@ -0,0 +1,17 @@
+﻿CREATE INDEX ocorrencia_id_ocorrencia_idx ON public.ocorrencia USING btree (id_ocorrencia);
+reindex table public.ocorrencia;
+
+CREATE INDEX tipo_evento_id_ocorrencia_idx ON public.ocorrencia_tipo_evento USING btree (id_ocorrencia);
+reindex table public.ocorrencia_tipo_evento;
+
+CREATE INDEX tipo_dano_identificado_id_ocorrencia_idx ON public.ocorrencia_tipo_dano_identificado USING btree (id_ocorrencia);
+reindex table public.ocorrencia_tipo_dano_identificado;
+
+CREATE INDEX tipo_fonte_informacao_id_ocorrencia_idx ON public.ocorrencia_tipo_fonte_informacao USING btree (id_ocorrencia);
+reindex table public.ocorrencia_tipo_fonte_informacao;
+
+CREATE INDEX instituicao_atuando_local_id_ocorrencia_idx ON public.ocorrencia_instituicao_atuando_local USING btree (id_ocorrencia);
+reindex table public.ocorrencia_instituicao_atuando_local;
+
+CREATE INDEX tipo_localizacao_id_ocorrencia_idx ON public.ocorrencia_tipo_localizacao USING btree (id_ocorrencia);
+reindex table public.ocorrencia_tipo_localizacao;
