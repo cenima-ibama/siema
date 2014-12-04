6a107e4db4fe78a79e34d91f199edfdaf9ba1f63
emmanuelporto <emmanuelporto@yahoo.com.br>
2014-12-04 16:32:46 -0300

    Replacement of id_ocorrencia by nro_ocorrencia the results of 'consultas'

--- assets/js/h5home.js
+++ assets/js/h5home.js
@@ -197,7 +197,7 @@
     rest = new H5.Rest({
       url: H5.Data.restURL,
       table: "vw_ocorrencia_consulta",
-      fields: "cast( id_ocorrencia AS text) AS id_ocorrencia, to_char(dt_registro,'DD/MM/YYYY') AS dt_registro, to_char(dt_ocorrencia,'DD/MM/YYYY') AS dt_ocorrencia, to_char(dt_primeira_obs,'DD/MM/YYYY') AS dt_primeira_obs, municipio, uf, array_to_string(origem,'; ') AS origem, array_to_string(tipo_evento,'; ') AS tipo_evento, array_to_string(tipos_danos_identificados,'; ') AS tipos_danos_identificados, array_to_string(institiuicoes_atuando_local,'; ') AS institiuicoes_atuando_local, array_to_string(tipos_fontes_informacoes,'; ') AS tipos_fontes_informacoes, dia_semana, dia_semana_primeira_obs, dia_semana_registro, periodo_ocorrencia, dt_ocorrencia_feriado, array_to_string(produtos_onu,'; ') AS produtos_onu, array_to_string(produtos_outro,'; ') AS produtos_outro, validado",
+      fields: "cast( id_ocorrencia AS text) AS id_ocorrencia, nro_ocorrencia, to_char(dt_registro,'DD/MM/YYYY') AS dt_registro, to_char(dt_ocorrencia,'DD/MM/YYYY') AS dt_ocorrencia, to_char(dt_primeira_obs,'DD/MM/YYYY') AS dt_primeira_obs, municipio, uf, array_to_string(origem,'; ') AS origem, array_to_string(tipo_evento,'; ') AS tipo_evento, array_to_string(tipos_danos_identificados,'; ') AS tipos_danos_identificados, array_to_string(institiuicoes_atuando_local,'; ') AS institiuicoes_atuando_local, array_to_string(tipos_fontes_informacoes,'; ') AS tipos_fontes_informacoes, dia_semana, dia_semana_primeira_obs, dia_semana_registro, periodo_ocorrencia, dt_ocorrencia_feriado, array_to_string(produtos_onu,'; ') AS produtos_onu, array_to_string(produtos_outro,'; ') AS produtos_outro, validado",
       parameters: query
     });
     console.log(query);
@@ -225,7 +225,7 @@
       }
       municipioUf = municipioUf.concat(dt.municipio, " - ", dt.uf);
       dataIncidente += " ";
-      return registroTemp[registroTemp.length] = new Array(dt.id_ocorrencia, dataIncidente, municipioUf, dt.origem, dt.tipo_evento, dt.produtos_onu, dt.produtos_outro, dt.tipos_danos_identificados, dt.institiuicoes_atuando_local, dt.tipos_fontes_informacoes, diaSemana, dt.periodo_ocorrencia, dt.dt_ocorrencia_feriado);
+      return registroTemp[registroTemp.length] = new Array(dt.nro_ocorrencia, dataIncidente, municipioUf, dt.origem, dt.tipo_evento, dt.produtos_onu, dt.produtos_outro, dt.tipos_danos_identificados, dt.institiuicoes_atuando_local, dt.tipos_fontes_informacoes, diaSemana, dt.periodo_ocorrencia, dt.dt_ocorrencia_feriado);
     });
     if (registroTemp.length > 0) {
       $("#optionsExport").show();
