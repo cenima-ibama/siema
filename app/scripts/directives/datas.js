d7dd405501caa3dfc94da05c7f71dba0744d20bf
Caio Castro <caio.castro@hexgis.com>
2015-04-29 09:53:35 -0300

    merging code with naldos version

--- app/scripts/directives/datas.js
+++ app/scripts/directives/datas.js
@@ -36,6 +36,49 @@ angular.module('estatisticasApp')
           $scope.datas[$semana] = new Date(temp[2],temp[1] - 1,temp[0]).getDay() + 1;
         }
 
+        $scope.$on('carregar_datas', function(event, data){
+            var temp;
+            if (data[0].dt_primeira_obs != null) {
+                temp = data[0].dt_primeira_obs.split("-");
+                $scope.datas.diaObservacao = temp[2] + "/" + temp[1] + "/" + temp[0];
+                $scope.updateSemana('diaObservacao','obsSemana');
+                $scope.datas.horaObservacao = data[0].hr_primeira_obs;
+                $scope.datas.obsPeriodo = data[0].periodo_primeira_obs;
+                $scope.datas.semObservacao = false;
+            } else {
+                $scope.datas.semObservacao = true;
+            }
+
+
+            if (data[0].dt_ocorrencia != null){
+                temp = data[0].dt_ocorrencia.split("-");
+                $scope.datas.diaIncidente = temp[2] + "/" + temp[1] + "/" + temp[0];
+                $scope.updateSemana('diaIncidente','incSemana');
+                $scope.datas.horaIncidente = data[0].hr_ocorrencia;
+                $scope.datas.incPeriodo = data[0].periodo_ocorrencia;
+                $scope.datas.feriado = data[0].dt_ocorrencia_feriado;
+                $scope.datas.semIncidente = false;
+            } else {
+                $scope.datas.semIncidente = true;
+            }
+        });
+
+        // $scope.$on('criar_datas', function(event, data){
+        //     $scope.datas.diaObservacao = "";
+        //     $scope.datas.horaObservacao = "";
+        //     $scope.datas.obsSemana = "";
+        //     $scope.datas.obsPeriodo = "";
+        //     $scope.datas.semObservacao = false;
+
+
+        //     $scope.datas.diaIncidente = "";
+        //     $scope.datas.horaIncidente = "";
+        //     $scope.datas.incSemana = "";
+        //     $scope.datas.incPeriodo = "";
+        //     $scope.datas.semIncidente = false;
+
+        // });
+
         // $scope.updateObsSemana = function() {
         //   var temp = $scope.datas.diaObservacao.split('/');
         //   $scope.datas.obsSemana = new Date(temp[2],temp[1] - 1,temp[0]).getDay() + 1;
