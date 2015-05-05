302dfee29086e4e5958452c77cb253cdf84878ab
dagnaldo <dagnaldo.silva@hexgis.com>
2015-05-05 17:48:53 -0300

    Adicionadas funcoes diversas a respeito dos produtos ONU - accordion 5

--- app/scripts/directives/produtos.js
+++ app/scripts/directives/produtos.js
@@ -7,40 +7,103 @@
  * # produtos
  */
 angular.module('estatisticasApp')
-  .directive('produtos', function () {
+  .directive('produtos', function ($compile, RestApi) {
     return {
       templateUrl: 'views/accordions/produtos.html',
       restrict: 'E',
       controller: function($scope){
 
+        $scope.lastEle = 1;
+        $scope.valueRows = [$scope.lastEle];
+
+        $scope.lastEleNaoOnu = 1001;
+        $scope.valueRowsNaoOnu = [$scope.lastEleNaoOnu];
+
+
         $scope.produtos.subPanel = $scope.oleo ? '(Itens V do Anexo II do Decreto nº 4.136 de 20 de fevereiro de 2002)' : '';
 
-        // $scope.localizacao.toggle = function() {
-        //   if ($scope.localizacao.show == 'in') {
-        //     $scope.localizacao.show = '';
-        //     $scope.datas.show = '';
-        //   } else {
-        //     $scope.localizacao.show = 'in';
-        //     $scope.datas.show = '';
-        //   }
+        // Funcoes para produtos ONU
+        $scope.addRowOnu = function(){
+            $scope.valueRows.push(++$scope.lastEle);
+        }
 
-        //   // console.log($scope.localizacao.show);
-        // };
+        $scope.deleteData = function(v){
+            var index = $scope.valueRows.indexOf(v);
+            if (index > -1)
+                 $scope.valueRows.splice(index, 1)
+        }
+        
+        $scope.saveData = function(i){
+            document.getElementById(i).addClass('disabled', 'disabled');
+        }
 
-        // $scope.loginIn = function(user, pass){
-        //   // $cookies.user = {user: user, password: pass};
-        //   RestApi.login({},{
-        //       username: user,
-        //       password: pass
-        //     },function success(data, status){
-        //       $scope.user = data.user;
-        //       Auth.setUser(data.user);
-        //       $location.path("/page2");
-        //     },function error(data, status){
-        //         console.log('!ERROR! ' + data);
-        //     }
-        //   );
-        // }
+        // Funcoes para produtos não ONU
+        $scope.addRowNaoOnu = function(){
+            $scope.valueRowsNaoOnu.push(++$scope.lastEleNaoOnu);
+        
+        }
+
+        $scope.deleteDataNaoOnu = function(v){
+            var index = $scope.valueRowsNaoOnu.indexOf(v);
+            if (index > -1) 
+                 $scope.valueRowsNaoOnu.splice(index, 1);
+        }
+
+        $scope.saveDataNaoOnu = function(i){
+            document.getElementById(i).addClass('disabled', 'disabled');
+        }
+
+        //Função para inicializar o popover
+        //Chamada no ng-init do html da diretiva
+        $scope.popover = function(){
+            $('[data-toggle="popover"]').popover({ html : true })
+            $("#addTipo").popover({
+                html : true, 
+                content: function() {
+                  return $('#popoverContent').html();
+                }
+            }).click(function(ev) {
+                 //this is workaround needed in order to make ng-click work inside of popover
+        
+                $compile($('.popover.in').contents())($scope);
+            });
+        }
+
+        $scope.validate = function(dado){
+            var data = ['SELECT', 'select', '*', 'FROM', 'from', '%', 'DROP', 'drop'];
+            var o=0;
+
+            angular.forEach(data, function(value, key){
+                if(dado == value)
+                    o++;
+                angular.forEach(dado, function(v, k){
+                    if(v == value)
+                        o++;
+                })
+            })
+
+            if(o==0)
+                return true;
+            else
+                return false;
+
+        }
+
+        $scope.addTipo = function(val){
+            var d = $scope.validate(val);
+            if(d){
+                RestApi.query({query: 'fontes'},
+                    function success(data, status){
+                        alert(data);
+                    }
+                );
+            } else {
+                alert('trying sql?');
+            }
+        }
+
+
+
 
       },
     };
